58ef770093c3b039cafbe9479f809ac2df1edf30
Rostislav <rostwik@gmail.com>
2022-07-30 13:50:41 +0300

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 
-from .models import Place
+from .models import Place, Image
 
-admin.site.register(Place)
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    # search_fields = ('town', 'address')
+    # readonly_fields = ['created_at']
+    list_display = (
+        'title',
+    )
+    # list_editable = ['new_building']
+    # list_filter = ['new_building', 'rooms_number', 'has_balcony']
+    # raw_id_fields = ['liked_by']
+    # inlines = [OwnerInline]
+
+
+@admin.register(Image)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+    )
