b235b1af5fe367ff26f9248eb982859ee81cccef
Rostislav <rostwik@gmail.com>
2022-09-04 21:09:44 +0300

    Add Inline for Place model

--- places/admin.py
+++ places/admin.py
@@ -3,22 +3,24 @@ from django.contrib import admin
 from .models import Place, Image
 
 
+class PlaceInline(admin.TabularInline):
+    model = Image
+    ordering = ['position']
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    # search_fields = ('town', 'address')
-    # readonly_fields = ['created_at']
+    inlines = [
+        PlaceInline,
+    ]
     list_display = (
-        'title',
+        'id', 'title',
     )
-    # list_editable = ['new_building']
-    # list_filter = ['new_building', 'rooms_number', 'has_balcony']
-    # raw_id_fields = ['liked_by']
-    # inlines = [OwnerInline]
 
 
 @admin.register(Image)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = (
-        'id',
-        'title',
+        'id', 'position', 'place'
     )
+    ordering = ['place', 'position']
--- places/models.py
+++ places/models.py
@@ -8,7 +8,14 @@ class Place(models.Model):
     coordinate_lng = models.FloatField(blank=True)
     coordinate_lat = models.FloatField(blank=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Image(models.Model):
-    title = models.CharField(max_length=200)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, blank=True, null=True)
     picture = models.ImageField(blank=True)
+    position = models.IntegerField(verbose_name='Позиция', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.id} {self.place.title}'
