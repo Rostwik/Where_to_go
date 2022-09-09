62230fc9a3ddfad4fca9e874a0a5565a81735fb0
Rostislav <rostwik@gmail.com>
2022-09-09 22:54:25 +0300

    Transferring settings for deployment

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -11,22 +11,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from environs import Env
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+env = Env()
+env.read_env()
+
+secret_key = env.str('SECRET_KEY')
+allowed_hosts = env.list('ALLOWED_HOSTS')
+
+SECRET_KEY = secret_key
+DEBUG = env.bool('DEBUG', True)
+ALLOWED_HOSTS = allowed_hosts
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)cc@)vne6ce7q9&_7*-!qi1-)ja8@c9@+j!2(@zfwg+22f9akp'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -125,7 +122,6 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
