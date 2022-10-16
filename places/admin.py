e20193d39f8be94d58cfb6346067d9b257e52823
Rostislav <rostwik@gmail.com>
2022-10-16 17:31:48 +0300

    Fixed errors in requirements

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,4 @@
-from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminMixin, SortableTabularInline, SortableAdminBase
+from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminBase
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
