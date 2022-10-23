dd762f8c1b06027bfb17b57569dc79ae3f7a908b
Rostislav <rostwik@gmail.com>
2022-10-23 16:07:04 +0300

    Fixed errors in requirements

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -116,7 +116,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "static"),
     # '/var/www/static/',
 ]
 
