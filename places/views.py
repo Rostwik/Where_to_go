a2beb9b2fff2091cedbb1c9b2c94e8386b5ec719
Rostislav <rostwik@gmail.com>
2023-02-05 22:35:27 +0300

    Move view to appropriate app

--- places/views.py
+++ places/views.py
@@ -1,3 +1,64 @@
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from places.models import Place, Image
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
+
+
+def index(request):
+    places = Place.objects.all()
+    features = []
+
+    for place in places:
+        features.append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        place.coordinate_lng,
+                        place.coordinate_lat
+                    ]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.id,
+                    "detailsUrl": reverse('api_place', args=(place.id,))
+                }
+            }
+        )
+
+    places_geojson = {
+        "type": "FeatureCollection",
+        "features": features
+    }
+
+    data = {"json": places_geojson}
+    return render(request, "index.html", context=data)
+
+
+def places(request, id):
+    place = get_object_or_404(Place, id=id)
+
+    place_imgs = [img.picture.url for img in Image.objects.filter(place=place).order_by('-position')]
+
+    place_detail = {
+        "title": place.title,
+        "imgs": place_imgs,
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.coordinate_lng,
+            "lat": place.coordinate_lat
+        }
+    }
+
+    return HttpResponse(
+        JsonResponse(
+            place_detail,
+            safe=False,
+            json_dumps_params={'ensure_ascii': False, 'indent': 2}
+        ),
+        content_type="application/json"
+    )
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from where_to_go import views, settings
+from where_to_go import settings
+from places import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
