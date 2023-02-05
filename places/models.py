4a8357cd73dee1d650ce5701da290ad31c29093d
Rostislav <rostwik@gmail.com>
2023-02-06 00:42:37 +0300

    Add related_name

--- places/models.py
+++ places/models.py
@@ -14,7 +14,7 @@ class Place(models.Model):
 
 
 class Image(models.Model):
-    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
     picture = models.ImageField()
     position = models.IntegerField(verbose_name='Позиция', blank=True, db_index=True, default=0)
 
